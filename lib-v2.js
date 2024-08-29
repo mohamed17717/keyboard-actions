7551c60ce66f7be547f6cb92826be39949abc88f
Mhmd <d3v.mhmd@gmail.com>
2024-08-29 14:48:45 +0300

    BEAUTIFY

--- lib-v2.js
+++ lib-v2.js
@@ -1,25 +1,25 @@
 class Type {
   static objectWithDefaultValue(defaultGenerator) {
     let handler = {
-      get: function(target, name) {
+      get: function (target, name) {
         name = name.toLowerCase(); // tolerant for case sensitivity
-        if (target.hasOwnProperty(name) ) {
+        if (target.hasOwnProperty(name)) {
           return target[name];
         } else {
           target[name] = defaultGenerator();
           return target[name];
         }
-      }
+      },
     };
-  
+
     return new Proxy({}, handler);
   }
-  
-  static uniqueArray(arr=[]) {
+
+  static uniqueArray(arr = []) {
     let handler = {
       get(target, prop) {
-        if (prop === 'push') {
-          return function(...args) {
+        if (prop === "push") {
+          return function (...args) {
             args.forEach((item) => {
               if (!target.includes(item)) {
                 Array.prototype.push.call(target, item);
@@ -27,16 +27,16 @@ class Type {
             });
             return target.length;
           };
-        } else if (prop === 'filter') {
+        } else if (prop === "filter") {
           return function (...args) {
             const filteredArray = Array.prototype.filter.apply(target, args);
             return Type.uniqueArray(filteredArray);
           };
         }
         return Reflect.get(target, prop);
-      }
+      },
     };
-  
+
     return new Proxy(arr, handler);
   }
 }
@@ -55,11 +55,11 @@ class KeyboardActionsConfig {
 
 class Utils {
   static onWindowChange(callback) {
-    window.addEventListener('blur', callback)
-    window.addEventListener('focus', callback)
-    window.addEventListener('focusin', callback)
-    window.addEventListener('focusout', callback)
-    window.addEventListener('close', callback)
+    window.addEventListener("blur", callback);
+    window.addEventListener("focus", callback);
+    window.addEventListener("focusin", callback);
+    window.addEventListener("focusout", callback);
+    window.addEventListener("close", callback);
   }
 }
 
@@ -70,7 +70,6 @@ class ClickManager {
     this.buttonActions = Type.objectWithDefaultValue(() => []);
   }
 
-  
   // Logging
   logKeys() {
     let keys = this.currentClickedButtons;
@@ -80,68 +79,64 @@ class ClickManager {
   // Events
   clear = (e) => {
     this.currentClickedButtons = Type.uniqueArray();
-  }
+  };
 
   keyDownMethod = (e) => {
     this.config.keepDefault || e.preventDefault();
     const keyName = this.getKeyName(e.key);
-    
+
     this.currentClickedButtons.push(keyName);
-    
+
     const actions = this.getKeyActions();
     this.config.preActionHook();
-    if(actions.length) {
-      actions.map(callback => callback(e));
+    if (actions.length) {
+      actions.map((callback) => callback(e));
       this.clear();
     }
     this.config.postActionHook();
 
-
     this.logKeys();
-  }
+  };
 
   keyUpMethod = (e) => {
     const keyName = this.getKeyName(e.key);
-    this.currentClickedButtons = this.currentClickedButtons.filter((key) => key !== keyName);
+    this.currentClickedButtons = this.currentClickedButtons.filter(
+      (key) => key !== keyName
+    );
 
     this.logKeys();
-  }
+  };
 
   // Key Actions
   getKeyActions() {
     let keys = this.currentClickedButtons;
-    return this.buttonActions[this.getCombinedKeysName(keys)]
+    return this.buttonActions[this.getCombinedKeysName(keys)];
   }
 
   // Naming
-  getCombinedKeysName(keys){
+  getCombinedKeysName(keys) {
     let keysCLone = [...keys];
-    if(this.config.isAutoKeyOrdering){
-      keysCLone.sort()
+    if (this.config.isAutoKeyOrdering) {
+      keysCLone.sort();
     }
 
-    return keysCLone.join(' + ')
+    return keysCLone.join(" + ");
   }
 
   getKeyName(key) {
     const ALIAS = { " ": "Space", "+": "Plus", "-": "Minus" };
     let keyName;
 
-    if (typeof key == 'string' && key.includes('+'))
-      key = key.split('+').map(k => k.trim())
+    if (typeof key == "string" && key.includes("+"))
+      key = key.split("+").map((k) => k.trim());
 
-    if (typeof key == 'string')
-      keyName = ALIAS[key] || key;
-    else if (Array.isArray(key)){
-      key = key.map(k => this.getKeyName(k));
+    if (typeof key == "string") keyName = ALIAS[key] || key;
+    else if (Array.isArray(key)) {
+      key = key.map((k) => this.getKeyName(k));
       keyName = this.getCombinedKeysName(key);
-    }
-    else
-      throw new Error('invalid key type');
+    } else throw new Error("invalid key type");
     return keyName;
   }
-
-
 }
 
 class EventsManager {
@@ -156,13 +151,14 @@ class EventsManager {
   }
 
   remove() {
-    this.events.forEach(([eventName, callback]) => this.config.element.removeEventListener(eventName, callback));
+    this.events.forEach(([eventName, callback]) =>
+      this.config.element.removeEventListener(eventName, callback)
+    );
     this.events = [];
   }
 }
 
 class KeyboardActions {
-
   constructor() {
     this.config = new KeyboardActionsConfig();
     this.clickManager = new ClickManager(this.config);
@@ -175,8 +171,11 @@ class KeyboardActions {
     if (this.isRunning) return;
 
     this.isRunning = true;
-    this.eventsManager.addEventListener('keydown', this.clickManager.keyDownMethod);
-    this.eventsManager.addEventListener('keyup', this.clickManager.keyUpMethod);
+    this.eventsManager.addEventListener(
+      "keydown",
+      this.clickManager.keyDownMethod
+    );
+    this.eventsManager.addEventListener("keyup", this.clickManager.keyUpMethod);
 
     Utils.onWindowChange(this.clickManager.clear);
   }
@@ -198,4 +197,3 @@ class KeyboardActions {
 }
 
 if (typeof exports != "undefined") module.exports = KeyboardActions;
-
